2d12cea5b33b03884207f695a243dbd1c01dd68e
Yumezuki <lolichan2018@gmail.com>
2024-01-04 23:21:21 +0700

    update

--- Lab04/src/Library.java
+++ Lab04/src/Library.java
@@ -4,43 +4,44 @@ public class Library {
     
     public void addBook(Book book, int slot) {
         switch (slot) {
-            case 1 -> book1 = book;
-            case 2 -> book2 = book;
-            case 3 -> book3 = book;
+            case 1:
+                book1 = book;
+                break;
+            case 2:
+                book2 = book;
+                break;
+            case 3:
+                book3 = book;
+                break;
+            default:
+                break;
         }
     }
     
     public void removeBook(int slot) {
         switch (slot) {
-            case 1 -> book1 = null;
-            case 2 -> book2 = null;
-            case 3 -> book3 = null;
+            case 1:
+                book1 = null;
+                break;
+            case 2:
+                book2 = null;
+                break;
+            case 3:
+                book3 = null;
+                break;
+            default:
+                break;
         }
     }
     
     public void printLibraryDetails() {
         System.out.println("Library: " + libraryName);
         System.out.println("");
-        System.out.println("Title: " + book1.title);
-        System.out.println("Author: " + book1.author);
-        System.out.println("Publisher: " + book1.publisher);
-        System.out.println("Year Published: " + book1.yearPublished);
-        System.out.println("Price: $" + book1.price);
-        System.out.println("Available: " + book1.isAvailable);
+        printBookDetails(book1);
         System.out.println("");
-        System.out.println("Title: " + book2.title);
-        System.out.println("Author: " + book2.author);
-        System.out.println("Publisher: " + book2.publisher);
-        System.out.println("Year Published: " + book2.yearPublished);
-        System.out.println("Price: $" + book2.price);
-        System.out.println("Available: " + book2.isAvailable);
+        printBookDetails(book2);
         System.out.println("");
-        System.out.println("Title: " + book3.title);
-        System.out.println("Author: " + book3.author);
-        System.out.println("Publisher: " + book3.publisher);
-        System.out.println("Year Published: " + book3.yearPublished);
-        System.out.println("Price: $" + book3.price);
-        System.out.println("Available: " + book3.isAvailable);
+        printBookDetails(book3);
     }
     
     public void checkBookAvailability(int slot) {
@@ -48,7 +49,7 @@ public class Library {
             System.out.println(book1.title + "is available");
         } else if (slot == 2 & book2 != null) {
             System.out.println(book2.title + "is available");
-        } else if (slot == 2 & book3 != null) {
+        } else if (slot == 3 & book3 != null) {
             System.out.println(book3.title + "is available");
         } else {
             System.out.println("Book in slot " + slot + " is not available");
@@ -58,11 +59,14 @@ public class Library {
     
     public void updateBookPrice(int slot, double newPrice) {
         if (slot == 1 & book1 != null) {
-            System.out.println("Updated price of " + book1.title + " to " + newPrice);
+            System.out.println("Updated price of " + book1.title + " to $" + newPrice + ".");
+            book1.price = newPrice;
         } else if (slot == 2 & book2 != null) {
-            System.out.println("Updated price of " + book2.title + " to " + newPrice);
-        } else if (slot == 2 & book3 != null) {
-            System.out.println("Updated price of " + book3.title + " to " + newPrice);
+            System.out.println("Updated price of " + book2.title + " to $" + newPrice + ".");
+            book2.price = newPrice;
+        } else if (slot == 3 & book3 != null) {
+            System.out.println("Updated price of " + book3.title + " to $" + newPrice + ".");
+            book3.price = newPrice;
         } else {
             System.out.println("No book in this slot.");
         }
@@ -75,7 +79,9 @@ public class Library {
             System.out.println("Publisher: " + book.publisher);
             System.out.println("Year Published: " + book.yearPublished);
             System.out.println("Price: $" + book.price);
-            System.out.println("Available: " + book.isAvailable);
+            System.out.print("Available: ");
+            if (book.isAvailable) {System.out.println("Yes");}
+            else {System.out.println("No");}
         } else {
             System.out.println("No book in this slot.");
         }
