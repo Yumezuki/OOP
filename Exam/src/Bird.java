b9c07dea050a8611c7cb6d6b633d8771bef2c953
Yumezuki <lolichan2018@gmail.com>
2024-01-22 01:15:11 +0700

    update

--- Exam/src/Bird.java
+++ Exam/src/Bird.java
@@ -2,7 +2,7 @@ public abstract class Bird extends Animal implements Flyable{
     public Bird(String name, int age){
         this.setName(name);
         this.setAge(age);
-        super.setPower(150);
+        this.setPower(150);
     }
     
     public abstract void wingAttack(Animal a);
